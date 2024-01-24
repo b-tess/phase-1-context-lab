103bf40cef60a7588630de355041a66dc4b73da0
Barbara Tess <barbaratessn@gmail.com>
2024-01-24 21:58:49 +0300

    Some functions now working

--- index.js
+++ index.js
@@ -1,5 +1,54 @@
 /* Your Code Here */
 
+// const uneditedEmployeeRecord = {
+//     firstName: '',
+//     familyName: '',
+//     title: '',
+//     payPerHour: 0,
+// }
+
+function createEmployeeRecord(arr) {
+    const employeeRecord = {
+        firstName: '',
+        familyName: '',
+        title: '',
+        payPerHour: 0,
+    }
+    let index = 0
+
+    for (let key in employeeRecord) {
+        employeeRecord[key] = arr[index++]
+    }
+
+    employeeRecord.timeInEvents = employeeRecord.timeOutEvents = []
+
+    return employeeRecord
+}
+
+function createEmployeeRecords(arr) {
+    const employeeRecords = []
+
+    arr.forEach((element) => {
+        const employeeRecordObject = createEmployeeRecord(element)
+
+        employeeRecords.push(employeeRecordObject)
+    })
+
+    return employeeRecords
+}
+
+function createTimeInEvent(string) {
+    const dateTimeArray = string.split(' ')
+    const timeInObject = {
+        type: 'TimeIn',
+        hour: parseInt(dateTimeArray[1]),
+        date: dateTimeArray[0],
+    }
+
+    this.timeInEvents.push(timeInObject)
+    return this
+}
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -14,10 +63,12 @@ const allWagesFor = function () {
         return e.date
     })
 
-    const payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+    const payable = eligibleDates.reduce(
+        function (memo, d) {
+            return memo + wagesEarnedOnDate.call(this, d)
+        }.bind(this),
+        0
+    ) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
 }
-
