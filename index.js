c3681bdc32bda239a42ad6b9a5094195a4509238
Barbara Tess <barbaratessn@gmail.com>
2024-01-25 11:22:44 +0300

    Hours and wages functions now working

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ function createEmployeeRecord(arr) {
         employeeRecord[key] = arr[index++]
     }
 
-    employeeRecord.timeInEvents = employeeRecord.timeOutEvents = []
+    employeeRecord.timeInEvents = []
+    employeeRecord.timeOutEvents = []
 
     return employeeRecord
 }
@@ -49,6 +50,56 @@ function createTimeInEvent(string) {
     return this
 }
 
+function createTimeOutEvent(string) {
+    const dateTimeArray = string.split(' ')
+    const timeOutObject = {
+        type: 'TimeOut',
+        hour: parseInt(dateTimeArray[1]),
+        date: dateTimeArray[0],
+    }
+
+    this.timeOutEvents.push(timeOutObject)
+    return this
+}
+
+//dateString
+const employeeRecord = createEmployeeRecord(['Julius', 'Caesar', 'General', 27])
+const timeIn = createTimeInEvent.call(employeeRecord, '2044-03-14 0900')
+const timeOut = createTimeOutEvent.call(employeeRecord, '2044-03-14 2100')
+
+const timeIn2 = createTimeInEvent.call(employeeRecord, '2044-03-15 0900')
+const timeOut2 = createTimeOutEvent.call(employeeRecord, '2044-03-15 1100')
+
+function hoursWorkedOnDate(dateString) {
+    //Keeping in mind => everyone checks in & out
+    let hoursWorked
+    if (this.timeInEvents.length > 1) {
+        timeInEventsArray = [...this.timeInEvents] //array of objects
+
+        timeInEventsArray.map((timeInEvent, index) => {
+            //imeinevent is an object here
+            if (timeInEvent.date === dateString) {
+                hoursWorked =
+                    (this.timeOutEvents[index].hour -
+                        this.timeInEvents[index].hour) /
+                    100
+                return hoursWorked
+            }
+        })
+    } else {
+        hoursWorked =
+            (this.timeOutEvents[0].hour - this.timeInEvents[0].hour) / 100
+    }
+
+    return hoursWorked
+}
+
+function wagesEarnedOnDate(dateString) {
+    const hoursWorked = hoursWorkedOnDate.call(this, dateString)
+    const wages = this.payPerHour * hoursWorked
+    return wages
+}
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -59,9 +110,12 @@ function createTimeInEvent(string) {
  */
 
 const allWagesFor = function () {
+    // console.log(this)
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
     })
+
+    // console.log('dates array:', eligibleDates)
 
     const payable = eligibleDates.reduce(
         function (memo, d) {
@@ -70,5 +124,9 @@ const allWagesFor = function () {
         0
     ) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
-    return payable
+    console.log('amount payable:', payable)
+
+    // return payable
 }
+
+allWagesFor.call(employeeRecord)
